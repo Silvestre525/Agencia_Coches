141da2bae93e6e09ccae15f62cc16383b154b59d
silvestre <silvestrevillar02@gmail.com>
2025-10-28 17:03:50 -0300

    add prefix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from models import Auto
 from schemas import AutoCreate,AutoRead
@@ -6,6 +6,7 @@ from database import SessionLocal, engine, Base
 from db_init import initialize_db_and_seed
 
 app = FastAPI()
+router = APIRouter(prefix="/api")
 
 @app.on_event("startup")
 def startup_event():
@@ -21,13 +22,13 @@ def get_db():
 
 
 #Get
-@app.get('/autos/', response_model=list[AutoRead])
+@router.get('/autos/', response_model=list[AutoRead], tags=["Autos"])
 def getAutos(db:Session = Depends(get_db)):
     autos = db.query(Auto).all()
     return autos
 
 #GetForId
-@app.get('/autos/{auto_id}',response_model=AutoRead)
+@router.get('/autos/{auto_id}',response_model=AutoRead, tags=["Autos"])
 def getAuto(auto_id:int, db:Session=Depends(get_db)):
     auto = db.query(Auto).filter(Auto.id == auto_id).first()
     if auto is None:
@@ -35,7 +36,7 @@ def getAuto(auto_id:int, db:Session=Depends(get_db)):
     return auto
 
 #Post
-@app.post('/autos/add',response_model=AutoCreate)
+@router.post('/autos/',response_model=AutoRead, tags=["Autos"])
 def addAuto(auto:AutoCreate, db:Session=Depends(get_db)):
     new_auto = Auto(marca=auto.marca,modelo=auto.modelo,anio=auto.anio,precio=auto.preco)
     db.add(new_auto)
@@ -44,17 +45,16 @@ def addAuto(auto:AutoCreate, db:Session=Depends(get_db)):
     return new_auto
 
 #Put
-@app.put('/autos/update/{auto_id}', response_model=AutoCreate)
+@router.put('/autos/{auto_id}', response_model=AutoRead, tags=["Autos"])
 def updateAuto(auto_id:int, auto:AutoCreate, db:Session=Depends(get_db)):
     auto_update = db.query(Auto).filter(Auto.id == auto_id).first()
 
     if auto_update is None:
         raise HTTPException(status_code=404,detail='Auto not found')
 
-    auto_update.marca = auto.marca
-    auto_update.modelo = auto.modelo
-    auto_update.anio = auto.anio 
-    auto_update.precio = auto.precio
+    auto_data = auto.dict()
+    for key, value in auto_data.items():
+        setattr(auto_update, key, value)
 
     db.commit()
     db.refresh(auto_update)
@@ -62,7 +62,7 @@ def updateAuto(auto_id:int, auto:AutoCreate, db:Session=Depends(get_db)):
     return auto_update
 
 #Delete
-@app.delete('/autos/delete/{auto_id}')
+@router.delete('/autos/{auto_id}', tags=["Autos"])
 def deleteAuto(auto_id:int, db:Session=Depends(get_db)):
     auto_delete = db.query(Auto).filter(Auto.id == auto_id).first()
 
@@ -70,5 +70,6 @@ def deleteAuto(auto_id:int, db:Session=Depends(get_db)):
         raise HTTPException(status_code=404,detail='Auto not found')
     db.delete(auto_delete)
     db.commit()
-    return {'message':'Auto deleted'}               
+    return {'message':'Auto deleted'}
 
+app.include_router(router)
